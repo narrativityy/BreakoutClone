3d88314df788dacfe5887ded49f69a097050c4ca
narrativityy <jake_crowley@comcast.net>
2025-01-08 21:33:14 -0800

    added paddle, input, and movement

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 import pygame
 
+# initialization
 pygame.init()
 screen = pygame.display.set_mode((800, 600), flags=pygame.SCALED, vsync=1)
 clock = pygame.time.Clock()
 running = True
 
+# paddle attributes
+paddle_width = 200
+paddle_height = 20
+paddle_left = screen.get_width() / 2 - paddle_width / 2
+paddle_top = screen.get_height() - paddle_height - 20
+paddle_speed = 800
+
+# game loop
 while running:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-  screen.fill('black')
+    # background
+    screen.fill('black')
 
-  pygame.display.flip()
+    # main logic
 
-  dt = clock.tick(60) / 1000
+    # drawing paddle
+    pygame.draw.rect(screen, 'white', (paddle_left, paddle_top, paddle_width, paddle_height), 0, 10)
+
+    # getting user input
+    keys = pygame.key.get_pressed()
+
+    # paddle movement
+    if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+        paddle_left -= paddle_speed * dt
+    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+        paddle_left += paddle_speed * dt
+
+    # resetting frame
+    pygame.display.flip()
+
+    # setting fps and delta time
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
